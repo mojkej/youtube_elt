2d93f9b85cd6cf1a0c8da841b21464b84cb12970
Jason Oyono <132920515+mojkej@users.noreply.github.com>
2025-10-08 21:18:24 +0200

    Refactor to fetch channel playlist ID by handle
    Replaces direct use of CHANNEL_ID with CHANNEL_HANDLE and adds a function to fetch the channel's uploads playlist ID using the YouTube Data API. The script now retrieves the playlist ID for a channel handle instead of video details for a channel ID.

--- video_stats.py
+++ video_stats.py
@@ -6,11 +6,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
-CHANNEL_ID = os.getenv("CHANNEL_ID")
+CHANNEL_HANDLE = os.getenv("CHANNEL_ID")
 
-URL = f'https://youtube.googleapis.com/youtube/v3/videos?part=ContentDetails&id={
-    CHANNEL_ID}&key={API_KEY}'
 
-response = requests.get(URL, timeout=10)
-data = response.json()
-print(data)
+def get_channel_playlist_id():
+    '''Fetches the channel ID for the given channel.'''
+    try:
+        URL = f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={
+            CHANNEL_HANDLE}&key={API_KEY}'
+        response = requests.get(URL, timeout=10)
+        data = response.json()
+        channel_playlist_id = data['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+        return channel_playlist_id
+    except requests.exceptions.RequestException as e:
+        raise e
+
+
+if __name__ == "__main__":
+    get_channel_playlist_id()
