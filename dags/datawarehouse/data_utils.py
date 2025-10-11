2dfc5970890a1b5f0f6183dadfbfaddb67969aca
Jason Oyono <132920515+mojkej@users.noreply.github.com>
2025-10-11 20:02:22 +0200

    Add schema and table management utilities for Postgres
    Introduces functions to create schemas and tables in a PostgreSQL database if they do not exist, and to retrieve video IDs from a specified table. These utilities help streamline database setup and data retrieval in the data warehouse pipeline.

--- dags/datawarehouse/data_utils.py
+++ dags/datawarehouse/data_utils.py
@@ -22,3 +22,63 @@ def close_postgres_connection(connection, cursor):
     """
     cursor.close()
     connection.close()
+
+
+def create_schema(schema_name):
+    """
+    Creates a new schema in the PostgreSQL database if it does not already exist.
+    """
+    connection, cursor = get_postgres_connection()
+    try:
+        cursor.execute(f"CREATE SCHEMA IF NOT EXISTS {schema_name};")
+        connection.commit()
+    except Exception as e:
+        connection.rollback()
+        raise e
+    finally:
+        close_postgres_connection(connection, cursor)
+
+
+def create_table(schema_name, table_name, table_sql):
+    """
+    Creates a table in the PostgreSQL database if it does not already exist.
+    """
+    connection, cursor = get_postgres_connection()
+    table_sql = f"""
+    CREATE TABLE IF NOT EXISTS {table_name} (
+        video_id VARCHAR(11) PRIMARY KEY NOT NULL,
+        title TEXT NOT NULL,
+        published_at TIMESTAMP NOT NULL,
+        view_count INT,
+        like_count INT,
+        comment_count INT,
+        duration TIME NOT NULL,);"""
+    try:
+        if schema_name == "staging":
+            cursor.execute(
+                f"CREATE TABLE IF NOT EXISTS {schema_name}.{table_name} ({table_sql});")
+        else:
+            cursor.execute(
+                f"CREATE TABLE IF NOT EXISTS {schema_name}.{table_name} ({table_sql});")
+        connection.commit()
+    except Exception as e:
+        connection.rollback()
+        raise e
+    finally:
+        close_postgres_connection(connection, cursor)
+
+
+def get_videos_ids_from_db(schema_name, table_name):
+    """
+    Retrieves all video IDs from the specified table in the PostgreSQL database.
+    """
+    connection, cursor = get_postgres_connection()
+    try:
+        cursor.execute(f"SELECT video_id FROM {schema_name}.{table_name};")
+        rows = cursor.fetchall()
+        video_ids = [row['video_id'] for row in rows]
+        return video_ids
+    except Exception as e:
+        raise e
+    finally:
+        close_postgres_connection(connection, cursor)
